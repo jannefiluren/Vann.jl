c49d87a9939d89ff6795529cd15520955ce9f1c7
jmgnve <jmg@nve.no>
2016-10-18 10:10:58 +0200

    Added a test file

---
+++ tst.jl
@@ -0,0 +1,22 @@
+
+import Vann
+using PyPlot
+
+# Load data
+
+date, tair, prec, q_obs, frac = Vann.load_data("data_atnasjo");
+
+# Parameters
+
+param_snow  = [0.0, 3.69, 1.02];
+param_hydro = [100., 0.8, 0.05, 0.05, 0.01, 1., 2., 30., 2.5];
+
+# Select model
+
+st_snow  = Vann.TinBasicType(param_snow, frac);
+st_hydro = Vann.HbvType(param_hydro, frac);
+
+q_sim = Vann.run_model(st_snow, st_hydro, date, tair, prec);
+
+plot(1:length(q_sim), q_sim, linewidth=2.0, 1:length(q_obs), q_obs, linewidth=1.0);
+title("Simulated (blue) and observed (green) runoff");
