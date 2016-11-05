5d99f1e33a6a2261d4b618a6d043be36e08e7661
jmgnve <jmg@nve.no>
2016-11-05 12:31:17 +0100

    Moved perturb_input to examples

--- examples/pfilter_nve_evaluation.jl
+++ examples/pfilter_nve_evaluation.jl
@@ -30,6 +30,29 @@ mkpath(path_save * "/valid_png")
 
 ################################################################################
 
+# Perturb input data for snow model
+
+function perturb_input(st_snow::SnowType, prec, tair, itime)
+
+  n = Uniform(0.5, 1.5);
+  prec_noise = rand(n, 1);
+
+  n = Normal(0.0, 2);
+  tair_noise = rand(n, 1);
+
+  # Assign inputs to snow model
+
+  for izone in eachindex(st_snow.prec)
+
+    st_snow.prec[izone] = prec[izone, itime] * prec_noise[1];
+    st_snow.tair[izone] = tair[izone, itime] + tair_noise[1];
+
+  end
+
+end
+
+################################################################################
+
 # Particle filter
 
 function run_filter(prec, tair, epot, q_obs, param_snow, param_hydro, frac, npart)
