e861bf870c843f28fa83e5b24921cc68437c750a
Jan Magnusson <jmg@nve.no>
2017-03-13 14:51:28 +0100

    Changed loading files

--- test/calib_tests.jl
+++ test/calib_tests.jl
@@ -8,32 +8,34 @@ using NLopt
 
 # Read data
 
-data = readdlm("../data_airgr/test_data.txt", ',', header = true);
+filename = joinpath(dirname(@__FILE__), "../data_airgr/test_data.txt")
 
-prec  = data[1][:,1];
-epot  = data[1][:,2];
-q_obs = data[1][:,3];
+data = readdlm(filename, ',', header = true)
 
-prec = transpose(prec);
-epot = transpose(epot);
+prec  = data[1][:,1]
+epot  = data[1][:,2]
+q_obs = data[1][:,3]
 
-frac = zeros(Float64, 1);
+prec = transpose(prec)
+epot = transpose(epot)
+
+frac = zeros(Float64, 1)
 
 # Select model
 
-st_hydro = Gr4jType(frac);
+st_hydro = Gr4jType(frac)
 
 # Run calibration
 
-res = run_model_calib(st_hydro, prec, epot, q_obs);
+res = run_model_calib(st_hydro, prec, epot, q_obs)
 
 # Parameter values
 
-param  = [257.238, 1.012, 88.235, 2.208];
+param  = [257.238, 1.012, 88.235, 2.208]
 
 # Test whether calibration gives correct answer
 
-param_range = get_param_range(st_hydro);
+param_range = get_param_range(st_hydro)
 
 println("Finished test calibration of Gr4j")
 println(param)
@@ -41,7 +43,7 @@ println(res)
 
 for iparam in eachindex(param)
 
-  range_param = param_range[iparam][2] - param_range[iparam][1];
+  range_param = param_range[iparam][2] - param_range[iparam][1]
 
   if !(param[iparam]-0.05*range_param < res[iparam] < param[iparam]+0.05*range_param)
     error("Calibration of gr4j resulted in wrong parameter values")
@@ -55,31 +57,33 @@ end
 
 # Load data
 
-date, tair, prec, q_obs, frac = load_data("../data_atnasjo", "Q_ref.txt");
+filename = joinpath(dirname(@__FILE__), "../data_atnasjo")
+
+date, tair, prec, q_obs, frac = load_data(filename, "Q_ref.txt")
 
 # Compute potential evapotranspiration
 
-epot = epot_zero(date);
+epot = epot_zero(date)
 
 # Select model
 
-st_snow = TinBasicType(frac);
-st_hydro = Gr4jType(frac);
+st_snow = TinBasicType(frac)
+st_hydro = Gr4jType(frac)
 
 # Run calibration
 
-res = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs);
+res = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs)
 
 # Parameter values
 
-param  = [0.0, 3.69, 1.02, 74.59, 0.81, 214.98, 1.24];
+param  = [0.0, 3.69, 1.02, 74.59, 0.81, 214.98, 1.24]
 
 # Test whether calibration gives correct answer
 
-param_range_snow  = get_param_range(st_snow);
-param_range_hydro = get_param_range(st_hydro);
+param_range_snow  = get_param_range(st_snow)
+param_range_hydro = get_param_range(st_hydro)
 
-param_range = vcat(param_range_snow, param_range_hydro);
+param_range = vcat(param_range_snow, param_range_hydro)
 
 println("Finished test calibration of TinBasic and Gr4j")
 println(param)
@@ -101,20 +105,22 @@ println(res)
 
 # Load data
 
-date, tair, prec, q_obs, frac = load_data("../data_atnasjo", "Q_ref.txt");
+filename = joinpath(dirname(@__FILE__), "../data_atnasjo")
+
+date, tair, prec, q_obs, frac = load_data(filename, "Q_ref.txt")
 
 # Compute potential evapotranspiration
 
-epot = epot_zero(date);
+epot = epot_zero(date)
 
 # Select model
 
-st_snow = TinStandardType(frac);
-st_hydro = Gr4jType(frac);
+st_snow = TinStandardType(frac)
+st_hydro = Gr4jType(frac)
 
 # Run calibration
 
-res = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs);
+res = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs)
 
 println("Finished test calibration of TinStandard and Gr4j")
 println(res)
