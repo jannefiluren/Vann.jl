4650c233aed2d0014a1002b24ce74c3bf5aebe08
Jan Magnusson <jmg@nve.no>
2017-03-13 14:46:23 +0100

    Changed file loading9

--- test/model_tests.jl
+++ test/model_tests.jl
@@ -8,18 +8,20 @@ using Base.Test
 
 # Read data
 
-data = readdlm("../data_airgr/test_data.txt", ',', header = true);
+filename = joinpath(dirname(@__FILE__), "../data_airgr/test_data.txt")
 
-prec  = data[1][:,1];
-epot  = data[1][:,2];
-q_obs = data[1][:,3];
+data = readdlm(filename, ',', header = true)
 
-prec = transpose(prec);
-epot = transpose(epot);
+prec  = data[1][:,1]
+epot  = data[1][:,2]
+q_obs = data[1][:,3]
 
-frac = zeros(Float64, 1);
+prec = transpose(prec)
+epot = transpose(epot)
 
-param  = [257.238, 1.012, 88.235, 2.208];
+frac = zeros(Float64, 1)
+
+param  = [257.238, 1.012, 88.235, 2.208]
 
 # Select model
 
@@ -44,27 +46,29 @@ println("Gr4j maximum error = " * string(err_max))
 
 # Load data
 
-date, tair, prec, q_obs, frac = load_data("../data_atnasjo", "Q_ref.txt");
+filename = joinpath(dirname(@__FILE__), "../data_atnasjo")
+
+date, tair, prec, q_obs, frac = load_data(filename, "Q_ref.txt")
 
 # Compute potential evapotranspiration
 
-epot = epot_zero(date);
+epot = epot_zero(date)
 
 # Parameters
 
-param_snow  = [0.0, 3.69, 1.02];
-param_hydro = [74.59, 0.81, 214.98, 1.24];
+param_snow  = [0.0, 3.69, 1.02]
+param_hydro = [74.59, 0.81, 214.98, 1.24]
 
 # Select model
 
-st_snow  = TinBasicType(param_snow, frac);
-st_hydro = Gr4jType(param_hydro, frac);
+st_snow  = TinBasicType(param_snow, frac)
+st_hydro = Gr4jType(param_hydro, frac)
 
-q_sim = run_model(st_snow, st_hydro, date, tair, prec, epot);
+q_sim = run_model(st_snow, st_hydro, date, tair, prec, epot)
 
 # Compute largest error
 
-err_max = maximum(map(abs, q_sim - q_obs));
+err_max = maximum(map(abs, q_sim - q_obs))
 
 println("Gr4j + TinBasic maximum error = " * string(err_max))
 
@@ -75,26 +79,28 @@ println("Gr4j + TinBasic maximum error = " * string(err_max))
 
 # Load data
 
-date, tair, prec, q_obs, frac = load_data("../data_atnasjo", "Q_ref.txt");
+filename = joinpath(dirname(@__FILE__), "../data_atnasjo")
+
+date, tair, prec, q_obs, frac = load_data(filename, "Q_ref.txt")
 
 # Compute potential evapotranspiration
 
-epot = epot_zero(date);
+epot = epot_zero(date)
 
 # Parameters
 
-param_snow  = [0.0, 3.69, 3.69, 0., 1.02];
-param_hydro = [74.59, 0.81, 214.98, 1.24];
+param_snow  = [0.0, 3.69, 3.69, 0., 1.02]
+param_hydro = [74.59, 0.81, 214.98, 1.24]
 
 # Select model
 
-st_snow  = TinStandardType(param_snow, frac);
-st_hydro = Gr4jType(param_hydro, frac);
+st_snow  = TinStandardType(param_snow, frac)
+st_hydro = Gr4jType(param_hydro, frac)
 
-q_sim = run_model(st_snow, st_hydro, date, tair, prec, epot);
+q_sim = run_model(st_snow, st_hydro, date, tair, prec, epot)
 
 # Compute largest error
 
-err_max = maximum(map(abs, q_sim - q_obs));
+err_max = maximum(map(abs, q_sim - q_obs))
 
 println("Gr4j + TinBasic maximum error = " * string(err_max))
