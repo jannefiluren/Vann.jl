5175386a47a697b1f39c8f22df7aab08dcb85b37
autodocs <autodocs>
2017-03-20 15:04:41 +0000

    build based on 3ac7726

--- latest/search_index.js
+++ latest/search_index.js
@@ -9,11 +9,99 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "index.html#Overview-1",
+    "location": "index.html#Vann.jl-1",
     "page": "Home",
-    "title": "Overview",
+    "title": "Vann.jl",
     "category": "section",
-    "text": "A Julia package for hydrological modelling.This package implements things"
+    "text": "A Julia package for hydrological modelling."
+},
+
+{
+    "location": "index.html#Package-Features-1",
+    "page": "Home",
+    "title": "Package Features",
+    "category": "section",
+    "text": "This package includes different model components for computing processes such as evapotranspiration, snow accumulation and melt, and routing of water through the subsurface. The model blocks can be combined in arbritraty combinations. The package additionally includes different data assimilation procedures for updating the state variables of the hydrological models."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "The package and so can be installed using:Pkg.clone(\"https://github.com/jmgnve/Vann.jl.git\")"
+},
+
+{
+    "location": "hyd_models.html#",
+    "page": "Hydrological models",
+    "title": "Hydrological models",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "hyd_models.html#Hydrological-model-components-1",
+    "page": "Hydrological models",
+    "title": "Hydrological model components",
+    "category": "section",
+    "text": "A Julia package for hydrological modelling."
+},
+
+{
+    "location": "hyd_models.html#Potential-evapotranspiration-1",
+    "page": "Hydrological models",
+    "title": "Potential evapotranspiration",
+    "category": "section",
+    "text": "Zero potential evapotranspirationMonthly potential evapotranspiration"
+},
+
+{
+    "location": "hyd_models.html#Snow-models-1",
+    "page": "Hydrological models",
+    "title": "Snow models",
+    "category": "section",
+    "text": "Simple temperature index modelAdvanced temperature index model"
+},
+
+{
+    "location": "hyd_models.html#Hydrological-models-1",
+    "page": "Hydrological models",
+    "title": "Hydrological models",
+    "category": "section",
+    "text": "Swedish hydrological modelFrench hydrological model"
+},
+
+{
+    "location": "data_assim.html#",
+    "page": "Data assimilation",
+    "title": "Data assimilation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "data_assim.html#Data-assimilation-methods-1",
+    "page": "Data assimilation",
+    "title": "Data assimilation methods",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "data_assim.html#Ensemble-Kalman-filter-1",
+    "page": "Data assimilation",
+    "title": "Ensemble Kalman filter",
+    "category": "section",
+    "text": "An example"
+},
+
+{
+    "location": "data_assim.html#Particle-filter-1",
+    "page": "Data assimilation",
+    "title": "Particle filter",
+    "category": "section",
+    "text": "An example"
 },
 
 ]}
