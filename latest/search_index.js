1c4acaa6247393bf98025785e8e5ab9ac4a80b67
autodocs <autodocs>
2017-03-21 15:27:11 +0000

    build based on 3600ef1

--- latest/search_index.js
+++ latest/search_index.js
@@ -33,6 +33,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "index.html#Input-variables-1",
+    "page": "Home",
+    "title": "Input variables",
+    "category": "section",
+    "text": "Input variables depending on the length of the time step, such as precipitation, should given in units mm/timestep For all models, the time step should be relative to daily inputs. Thus, if hourly input data is used, the model time step should equal 1/24."
+},
+
+{
     "location": "hyd_models.html#",
     "page": "Hydrological models",
     "title": "Hydrological models",
@@ -45,31 +53,23 @@ var documenterSearchIndex = {"docs": [
     "page": "Hydrological models",
     "title": "Hydrological model components",
     "category": "section",
-    "text": "A Julia package for hydrological modelling."
+    "text": ""
 },
 
 {
-    "location": "hyd_models.html#Potential-evapotranspiration-1",
+    "location": "hyd_models.html#GR4J-model-1",
     "page": "Hydrological models",
-    "title": "Potential evapotranspiration",
+    "title": "GR4J model",
     "category": "section",
-    "text": "Zero potential evapotranspirationMonthly potential evapotranspiration"
+    "text": "Gr4j\nGr4j()\nGr4j(param)\nget_param_range(mdata::Gr4j)\ninit_states(mdata::Gr4j)\nassign_param(mdata::Gr4j, param::Array{Float64,1})\nhydro_model(mdata::Gr4j)"
 },
 
 {
-    "location": "hyd_models.html#Snow-models-1",
+    "location": "hyd_models.html#HBV-model-1",
     "page": "Hydrological models",
-    "title": "Snow models",
+    "title": "HBV model",
     "category": "section",
-    "text": "Simple temperature index modelAdvanced temperature index model"
-},
-
-{
-    "location": "hyd_models.html#Hydrological-models-1",
-    "page": "Hydrological models",
-    "title": "Hydrological models",
-    "category": "section",
-    "text": "Swedish hydrological modelFrench hydrological model"
+    "text": ""
 },
 
 {
