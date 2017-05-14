aabf1902671def0a05ce670c53c12418a486e2c1
jmgnve <jmg@nve.no>
2017-05-14 20:24:20 +0200

    Fixed forecast

---
+++ applications/data_assim_forecast.jl
@@ -0,0 +1,207 @@
+
+# Add packages
+
+using Vann
+using DataAssim
+using ExcelReaders
+using DataFrames
+using PyPlot
+
+# Settings
+
+file_exper  = "C:/Work/Studies/vann/experiments.xlsx"
+path_inputs = "C:/Work/Studies/vann/data/norway"
+path_save   = "C:/Work/Studies/vann/filter_forecast"
+
+df_exper = readxlsheet(DataFrame, file_exper, "Filter")
+
+# Run one experiment
+
+function run_single_experiment(path_inputs, path_save, df_exper, i_exper)
+
+  # Folder for saving results
+
+  path_save = joinpath(path_save, "experiment_" * string(i_exper))
+
+  mkpath(path_save * "/tables")
+  mkpath(path_save * "/figures")
+
+  # Get settings
+
+  opt = Dict()
+
+  opt["epot_choice"]   = parse(df_exper[:epot_choice][i_exper])
+  opt["snow_choice"]   = parse(df_exper[:snow_choice][i_exper])
+  opt["hydro_choice"]  = parse(df_exper[:hydro_choice][i_exper])
+
+  opt["filter_choice"] = parse(df_exper[:filter_choice][i_exper])
+  opt["nens"]          = round(Int64, df_exper[:nens][i_exper])
+  opt["test_forecast"] = parse(df_exper[:test_forecast][i_exper])
+
+  opt["warmup"]        = round(Int64, df_exper[:warmup][i_exper])
+  opt["tstep"]         = df_exper[:tstep][i_exper]
+  opt["date_start"]    = df_exper[:date_start][i_exper]
+  opt["date_stop"]     = df_exper[:date_stop][i_exper]
+
+  opt["path_inputs"]   = path_inputs
+  opt["path_save"]     = path_save
+
+  # Run for all stations
+
+  run_all_stations(opt)
+
+end
+
+# Run over all stations
+
+function run_all_stations(opt)
+
+  # Empty dataframes for summary statistics
+
+  df_summary = DataFrame()
+
+  header = [:Station :NSE_CAL :KGE_CAL :NSE_DA :KGE_DA]
+
+  for item in header
+    df_summary[item] = []
+  end 
+
+  df_forecast = DataFrame()
+
+  header = [:Station :NSE1 :NSE2 :NSE3 :NSE4 :NSE5 :NSE6 :NSE7 :KGE1 :KGE2 :KGE3 :KGE4 :KGE5 :KGE6 :KGE7]
+
+  for item in header
+    df_forecast[item] = []
+  end 
+  
+  # Loop over all watersheds
+
+  dir_all = readdir(opt["path_inputs"])
+
+  for dir_cur in dir_all
+
+    # Load data
+
+    date, tair, prec, q_obs, frac = load_data("$(opt["path_inputs"])/$dir_cur")
+
+    # Crop data
+
+    date, tair, prec, q_obs = crop_data(date, tair, prec, q_obs, opt["date_start"], opt["date_stop"])
+
+    # Compute potential evapotranspiration
+
+    epot = eval(Expr(:call, opt["epot_choice"], date))
+
+    # Precipitation correction step
+
+    if sum(~isnan(q_obs)) > (3*365)
+
+      ikeep = ~isnan(q_obs)
+
+      prec_tmp = sum(prec .* repmat(frac, 1, size(prec,2)), 1)
+      
+      prec_sum = sum(prec_tmp[ikeep])
+      q_sum = sum(q_obs[ikeep])
+      epot_sum = sum(epot[ikeep])
+
+      pcorr = (q_sum + 0.5 * epot_sum) / prec_sum
+
+      prec = pcorr * prec
+
+    else
+
+      warn("Not enough runoff data for calibration (see folder $dir_cur)")
+      continue
+
+    end
+
+    # Initilize model
+
+    st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], frac))
+    st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1]))
+
+    # Run calibration
+
+    param_snow, param_hydro = run_model_calib(st_snow, st_hydro, date, tair, prec, epot, q_obs;
+                                              warmup = opt["warmup"])
+
+    # Run model with optimal parameters
+
+    st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], param_snow, frac))
+    st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1], param_hydro))
+
+    q_cal = run_model(st_snow, st_hydro, date, tair, prec, epot)
+
+    # Run model with filter filter
+
+    st_snow = eval(Expr(:call, opt["snow_choice"], opt["tstep"], date[1], param_snow, frac))
+    st_hydro = eval(Expr(:call, opt["hydro_choice"], opt["tstep"], date[1], param_hydro))
+
+    kw_args = Expr(:kw, :test_forecast, opt["test_forecast"])
+
+    q_da, q_sim_forecast, q_obs_forecast = eval(Expr(:call, opt["filter_choice"], st_snow, st_hydro, prec, tair, epot, q_obs, opt["nens"], kw_args))
+  
+    # Add data to summary tables
+
+    station = dir_cur[1:end-5]
+    nse_cal = nse(q_cal[opt["warmup"]:end], q_obs[opt["warmup"]:end])
+    kge_cal = kge(q_cal[opt["warmup"]:end], q_obs[opt["warmup"]:end])
+    nse_da  = nse(q_da[opt["warmup"]:end, 1], q_obs[opt["warmup"]:end])
+    kge_da  = kge(q_da[opt["warmup"]:end, 1], q_obs[opt["warmup"]:end])
+
+    push!(df_summary, [station; nse_cal; kge_cal; nse_da; kge_da])
+
+    nse_forecast = [nse(q_sim_forecast[opt["warmup"]:end, i], q_obs_forecast[opt["warmup"]:end, i]) for i in 1:size(q_sim_forecast, 2)]
+    kge_forecast = [kge(q_sim_forecast[opt["warmup"]:end, i], q_obs_forecast[opt["warmup"]:end, i]) for i in 1:size(q_sim_forecast, 2)]
+
+    push!(df_forecast, [station; nse_forecast; kge_forecast])
+    
+    # Add time series to file
+
+    q_obs = round(q_obs, 2)
+    q_sim = round(q_da[:, 1], 2)
+    q_min = round(q_da[:, 2], 2)
+    q_max = round(q_da[:, 3], 2)
+
+    df_res = DataFrame(date = date, q_obs = q_obs, q_sim = q_sim, q_min = q_min, q_max = q_max)
+
+    file_name = joinpath(opt["path_save"], "tables", dir_cur[1:end-5] * "_station.txt")
+
+    writetable(file_name, df_res, quotemark = '"', separator = '\t')
+
+    # Save plots
+
+    ioff()
+
+    fig = plt[:figure](figsize = (12,7))
+
+    plt[:style][:use]("ggplot")
+
+    plt[:plot](date, q_obs, linewidth = 1.2, color = "k", label = "Observed", zorder = 1)
+    plt[:fill_between](date, q_max, q_min, facecolor = "r", edgecolor = "r", label = "Simulated", alpha = 0.55, zorder = 2)
+    plt[:ylabel]("Runoff (mm/day)")
+
+    plt[:legend]()
+    
+    file_name = joinpath(opt["path_save"], "figures", dir_cur[1:end-5] * "_station.png")
+    
+    savefig(file_name, dpi = 600)
+    close(fig)
+
+  end
+
+  # Write summary tables to files
+
+  writetable(string(opt["path_save"], "/summary_table.txt"), df_summary, quotemark = '"', separator = '\t')
+  writetable(string(opt["path_save"], "/forecast_table.txt"), df_forecast, quotemark = '"', separator = '\t')
+
+end
+
+
+# Run all experiments
+
+for i_exper = 1:size(df_exper, 1)
+
+  run_single_experiment(path_inputs, path_save, df_exper, i_exper)
+
+end
--- src/utils_data_assim.jl
+++ src/utils_data_assim.jl
@@ -24,11 +24,11 @@ end
 
 
 """
-    particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
+    particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, nens)
 
 Run the particle filter for any combination of snow and hydrological routing model.
 """
-function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
+function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, nens; test_forecast = false)
 
     srand(1)
 
@@ -39,21 +39,30 @@ function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
 
     # Initilize state variables
 
-    st_snow  = [deepcopy(st_snow) for i in 1:npart]
-    st_hydro = [deepcopy(st_hydro) for i in 1:npart]
+    st_snow  = [deepcopy(st_snow) for i in 1:nens]
+    st_hydro = [deepcopy(st_hydro) for i in 1:nens]
 
     # Initilize particles
 
-    wk = ones(npart) / npart
+    wk = ones(nens) / nens
 
-    # Run model
+    # Allocate arrays
 
-    q_sim = zeros(Float64, npart)
-    q_res = zeros(Float64, ntimes, 3)
+    q_res  = zeros(ntimes, 3)
+    q_sim  = zeros(1, nens)
+
+    # Arrays for forecast
+
+    if test_forecast
+        q_sim_forecast = []
+        q_obs_forecast = []
+    end
+
+    # Start time loop
 
     for itime = 1:ntimes
 
-        for ipart = 1:npart
+        for ipart = 1:nens
 
             perturb_input(st_snow[ipart], prec, tair, itime)
 
@@ -71,7 +80,7 @@ function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
 
         if true
 
-            for ipart = 1:npart
+            for ipart = 1:nens
 
                 wk[ipart] = pdf(Normal(q_obs[itime], max(0.1 * q_obs[itime], 0.1)), q_sim[ipart]) * wk[ipart]
 
@@ -80,14 +89,14 @@ function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
             if sum(wk) > 0.0
                 wk = wk / sum(wk)
             else
-                wk = ones(npart) / npart
+                wk = ones(nens) / nens
             end
 
             # Perform resampling
 
             Neff = 1 / sum(wk.^2)
 
-            if round(Int64, Neff) < round(Int64, npart * 0.8)
+            if round(Int64, Neff) < round(Int64, nens * 0.8)
 
                 println("Resampled at step: $itime")
 
@@ -96,7 +105,7 @@ function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
                 st_snow  = [deepcopy(st_snow[i]) for i in indx]
                 st_hydro = [deepcopy(st_hydro[i]) for i in indx]
 
-                wk = ones(npart) / npart
+                wk = ones(nens) / nens
 
             end
 
@@ -104,13 +113,37 @@ function particle_filter(st_snow, st_hydro, prec, tair, epot, q_obs, npart)
 
         # Store results
 
-        q_res[itime, 1] = sum(wk .* q_sim)
+        q_res[itime, 1] = sum(wk[:] .* q_sim[:])
         q_res[itime, 2] = minimum(q_sim)
         q_res[itime, 3] = maximum(q_sim)
 
+        # Run forecast
+
+        if test_forecast
+            
+            tmp_sim, tmp_obs = run_forecast(st_snow, st_hydro, prec, tair, epot, q_obs, itime, nens, 7)
+
+            if isempty(q_sim_forecast)
+                q_sim_forecast = tmp_sim
+                q_obs_forecast = tmp_obs
+            else
+                q_sim_forecast = hcat(q_sim_forecast, tmp_sim)
+                q_obs_forecast = hcat(q_obs_forecast, tmp_obs)
+            end
+
+        end
+
     end
 
-    return(q_res)
+    # Return results
+
+    if test_forecast
+        q_sim_forecast = transpose(q_sim_forecast)
+        q_obs_forecast = transpose(q_obs_forecast)
+        return q_res, q_sim_forecast, q_obs_forecast
+    else
+        return q_res
+    end
 
 end
 
@@ -121,79 +154,160 @@ end
 Run the ensemble Kalman filter for any combination of snow and hydrological
 routing model.
 """
-function enkf_filter(st_snow, st_hydro, prec, tair, epot, q_obs, nens)
+function enkf_filter(st_snow, st_hydro, prec, tair, epot, q_obs, nens; test_forecast = false)
 
-  srand(1)
+    srand(1)
 
-  # Number of elevation bands (rows) and time steps (cols)
+    # Number of elevation bands (rows) and time steps (cols)
 
-  nzones = size(prec, 1)
-  ntimes = size(prec, 2)
+    nzones = size(prec, 1)
+    ntimes = size(prec, 2)
 
-  # Initilize state variables
+    # Initilize state variables
 
-  st_snow  = [deepcopy(st_snow) for i in 1:nens]
-  st_hydro = [deepcopy(st_hydro) for i in 1:nens]
+    st_snow  = [deepcopy(st_snow) for i in 1:nens]
+    st_hydro = [deepcopy(st_hydro) for i in 1:nens]
 
-  # Allocate arrays
+    # Allocate arrays
 
-  q_res  = zeros(Float64, ntimes, 3)
-  q_sim  = zeros(Float64, 1, nens)
+    q_res  = zeros(ntimes, 3)
+    q_sim  = zeros(1, nens)
 
-  # Start time Loop
+    # Arrays for forecast
 
-  for itime = 1:ntimes
-
-    # Perturb inputs and run models
-
-    for iens = 1:nens
-
-      perturb_input(st_snow[iens], prec, tair, itime)
-
-      run_timestep(st_snow[iens])
-
-      get_input(st_snow[iens], st_hydro[iens], epot, itime)
-
-      run_timestep(st_hydro[iens])
-
-      q_sim[iens] = st_hydro[iens].q_sim
-
+    if test_forecast
+        q_sim_forecast = []
+        q_obs_forecast = []
     end
 
-    # Run filter part
+    # Start time loop
 
-    if q_obs[itime] >= 0
+    for itime = 1:ntimes
 
-      # Perturb observations
+        # Perturb inputs and run models
 
-      sigma = max(0.1 * q_obs[itime], 0.1)
+        for iens = 1:nens
 
-      obs_ens = q_obs[itime] + sigma * randn(Float64, 1, nens)
+            perturb_input(st_snow[iens], prec, tair, itime)
 
-      # Update states of hydrological model
+            run_timestep(st_snow[iens])
 
-      enkf_hydro(st_hydro, obs_ens, q_sim)
+            get_input(st_snow[iens], st_hydro[iens], epot, itime)
 
-      # Update states of snow model
+            run_timestep(st_hydro[iens])
 
-      enkf_snow(st_snow, obs_ens, q_sim)
+            q_sim[iens] = st_hydro[iens].q_sim
 
-      # Update simulated discharge
+        end
 
-      q_sim  = enkf(q_sim, obs_ens, q_sim)
+        # Run filter part
 
-      q_sim[q_sim .< 0] = 0.
+        if q_obs[itime] >= 0
+
+            # Perturb observations
+
+            sigma = max(0.1 * q_obs[itime], 0.1)
+
+            obs_ens = q_obs[itime] + sigma * randn(Float64, 1, nens)
+
+            # Update states of hydrological model
+
+            enkf_hydro(st_hydro, obs_ens, q_sim)
+
+            # Update states of snow model
+
+            enkf_snow(st_snow, obs_ens, q_sim)
+
+            # Update simulated discharge
+
+            q_sim  = enkf(q_sim, obs_ens, q_sim)
+
+            q_sim[q_sim .< 0] = 0.
+
+        end
+
+        # Store results
+
+        q_res[itime, 1] = mean(q_sim)
+        q_res[itime, 2] = minimum(q_sim)
+        q_res[itime, 3] = maximum(q_sim)
+
+        # Run forecast
+
+        if test_forecast
+            
+            tmp_sim, tmp_obs = run_forecast(st_snow, st_hydro, prec, tair, epot, q_obs, itime, nens, 7)
+
+            if isempty(q_sim_forecast)
+                q_sim_forecast = tmp_sim
+                q_obs_forecast = tmp_obs
+            else
+                q_sim_forecast = hcat(q_sim_forecast, tmp_sim)
+                q_obs_forecast = hcat(q_obs_forecast, tmp_obs)
+            end
+
+        end
 
     end
+    
+    # Return results
 
-    # Store results
-
-    q_res[itime, 1] = mean(q_sim)
-    q_res[itime, 2] = minimum(q_sim)
-    q_res[itime, 3] = maximum(q_sim)
-
-  end
-
-  return(q_res)
+    if test_forecast
+        q_sim_forecast = transpose(q_sim_forecast)
+        q_obs_forecast = transpose(q_obs_forecast)
+        return q_res, q_sim_forecast, q_obs_forecast
+    else
+        return q_res
+    end
 
 end
+
+
+"""
+    run_forecast(st_snow, st_hydro, prec, tair, epot, q_obs, itime, nens, ndays)
+
+Run forecast
+"""
+function run_forecast(st_snow, st_hydro, prec, tair, epot, q_obs, itime, nens, ndays)
+
+    # Run forecast for ndays
+
+    q_sim_forecast = [NaN for i in 1:ndays]
+    q_obs_forecast = [NaN for i in 1:ndays]
+    
+    if itime < (length(q_obs) - ndays - 1)
+
+        st_snow  = [deepcopy(st_snow[i]) for i in 1:nens]
+        st_hydro = [deepcopy(st_hydro[i]) for i in 1:nens]
+        
+        q_sim = zeros(nens)
+
+        for itmp in (itime+1):(itime+ndays)
+
+            # Perturb inputs and run models
+
+            for iens = 1:nens
+
+                perturb_input(st_snow[iens], prec, tair, itmp)
+
+                run_timestep(st_snow[iens])
+
+                get_input(st_snow[iens], st_hydro[iens], epot, itmp)
+
+                run_timestep(st_hydro[iens])
+
+                q_sim[iens] = st_hydro[iens].q_sim
+                                
+            end
+
+            q_sim_forecast[itmp-itime] = mean(q_sim)
+            q_obs_forecast[itmp-itime] = q_obs[itmp]
+
+        end
+
+    end
+
+    return q_sim_forecast, q_obs_forecast
+
+end
+
